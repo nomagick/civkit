0a7fa21a0a3f5f67c15a868fa545848bc03939f1
Yanlong Wang <yanlong.wang@naiver.org>
2021-08-22 15:37:04 +0800

    rebuild

--- build/utils/property-injector.d.ts
+++ build/utils/property-injector.d.ts
@@ -1,3 +1,3 @@
 import type { container as DIContainer, InjectionToken } from 'tsyringe';
-export declare function propertyInjectorFactory(container: typeof DIContainer): <T = any>(token: InjectionToken<T>) => (tgt: any, key: string) => void;
+export declare function propertyInjectorFactory(container: typeof DIContainer): <T = any>(token?: InjectionToken<T> | undefined) => (tgt: any, key: string | symbol) => void;
 //# sourceMappingURL=property-injector.d.ts.map
--- build/utils/property-injector.js
+++ build/utils/property-injector.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.propertyInjectorFactory = void 0;
 function propertyInjectorFactory(container) {
     return function injectionDecorator(token) {
-        const result = container.resolve(token);
         return function injectionDecoratorFunc(tgt, key) {
+            const result = container.resolve(token || Reflect.getMetadata('design:type', tgt, key));
             Object.defineProperty(tgt, key, { value: result });
             return;
         };
