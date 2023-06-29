129a08592341342a1e52b2d18a958a64200cc8ce
Yanlong Wang <yanlong.wang@naiver.org>
2023-06-30 04:17:08 +0800

    fix: openapi

--- src/civ-rpc/openapi.ts
+++ src/civ-rpc/openapi.ts
@@ -1150,7 +1150,7 @@ export class OpenAPIManager {
                     const codeKey = `${wrappedTpm?.code || 200}`;
                     const codeTypeKey = `${codeKey}::${wrappedTpm?.contentType || 'application/json'}`;
                     const codeTypeValue = codeTypeMap.get(codeTypeKey);
-                    const partialSchema = this.propOptionsLikeToOpenAPISchema(wrappedRpcOptions, 'output');
+                    const partialSchema = this.autoTypesToOpenAPISchema(wrappedRpcOptions, 'output');
                     this.applySceneMeta(partialSchema, rpcOptions, 'response');
                     this.consumeIncompatibles(partialSchema);
                     if (codeTypeValue) {
